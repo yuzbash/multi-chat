482696e947205a3a92fc25cd11870a59bc451232
AnastasiaStep <snastya-13@yandex.ru>
2017-12-17 16:03:42 +0300

    added registration option

--- TcpClient/LoginWindow.xaml.cs
+++ TcpClient/LoginWindow.xaml.cs
@@ -83,5 +83,68 @@ namespace MyTcpClient
                 LabelLoginResult.Content = "Error with parametres";
             }
         }
+
+        private void ButtonRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress serverIp;
+            int serverPort;
+            string userName;
+            string password;
+            try
+            {
+                serverIp = IPAddress.Parse(TbServerIP.Text);
+                serverPort = Int32.Parse(TbServerPort.Text);
+                userName = TbUserName.Text;
+                password = TbUserPassword.Text;
+            }
+            catch
+            {
+                LabelLoginResult.Content = "Error with ip or port";
+                return;
+            }
+            Client client;
+            try
+            {
+                client = new Client(serverPort, serverIp, userName);
+            }
+            catch
+            {
+                LabelLoginResult.Content = "Error with creating client";
+                return;
+            }
+            try
+            {
+                if (client.Connect()) 
+                {
+                    Registration reg = new Registration();
+                    reg.ShowDialog();
+                    if (reg.IsOk)
+                    {
+                        string msg = reg.ResultString;
+                        client.SendServiceMessage(msg);
+                        string answer = client.RecieveMessage();
+                        if (answer == "true")
+                        {
+
+                        }
+                        else
+                        {
+                            LabelLoginResult.Content = "Error with registration. Maybe such user exists";
+                        }
+
+                    }
+                }
+                else
+                {
+                    client.Close();
+                    LabelLoginResult.Content = "Error with connection";
+                }
+            }
+            catch
+            {
+                LabelLoginResult.Content = "Error with parametres";
+            }
+                   
+        }
     }
 }
---
+++ TcpClient/Registration.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyTcpClient
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public bool IsOk{get; set;}
+        public string ResultString { get; set; }
+        public Registration()
+        {
+            InitializeComponent();
+            IsOk = false;
+        }
+
+        private void ButtonRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBuserNameReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter user name";
+                return;
+            }
+            if (TBpasswordReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter password";
+                return;
+            }
+            if (TBsurnameReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter surname";
+                return;
+            }
+            if (TBpassportNameReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter your real name";
+                return;
+            }
+            if (TBemailReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter email";
+                return;
+            }
+            if (TBcityReg.Text == "")
+            {
+                LabelResultReg.Content = "Please, enter city";
+                return;
+            }
+            string resString = "reg:" + 
+                "user:" + TBuserNameReg.Text +
+                "pswd:" + TBpasswordReg + 
+                "srnm:" + TBsurnameReg.Text+
+                "pasp:" + TBpassportNameReg.Text +
+                "emal:" + TBemailReg.Text +
+                "city:" + TBcityReg.Text;
+            ResultString = resString;
+            IsOk = true;
+        }
+    }
+}
